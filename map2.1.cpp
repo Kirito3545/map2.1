36506487559fb23f1d5ca7d67345f099448d96a7
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-08-28 21:46:34 +0300

    Initial commit

---
+++ map2.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <mutex>
+
+using namespace std::chrono_literals;
+std::atomic<int> clientsCount = 1;
+
+void clientsThread() {
+	while (clientsCount != 10) {
+		++clientsCount;
+		std::cout << "clientsCount from clientsThread: " << clientsCount << "\n";
+	}
+}
+
+void operatorTread() {
+	while (clientsCount != 0) {	
+		--clientsCount;
+		std::cout << "clientsCount from operatorTread: " << clientsCount << "\n";
+	}
+
+}
+int main() {
+	std::thread t1(clientsThread);
+	std::thread t2(operatorTread);
+	t1.join();
+	t2.join();
+	return 0;
+}
